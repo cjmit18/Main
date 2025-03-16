fb25cba993758d16fe394bc360955bdf845f429a
CJ <cj.mitchelljr@yahoo.com>
2025-03-16 12:36:47 -0500

    Added strings,bools and new main functions

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 import random
-import tkinter as tk
-roots = tk.TK()
 def computer_choice() -> str:
     choice = random.randint(1,3)
     if choice == 1:
@@ -9,15 +7,19 @@ def computer_choice() -> str:
         return "paper"
     elif choice == 3:
         return "scissors"
-def user_choice() -> str:
+def user_choice() -> str: 
       return input("Rock, Paper, or Scissors? ").lower()
-def main():
+def running_game() -> tuple:
     wins: int = 0
     losses: int = 0
     ties: int = 0
+    win: bool = None
+    lose: bool = None
+    tie: bool = None
     while True:
         user: str = user_choice()
         computer: str = computer_choice()
+        win, lose, tie = False, False, False 
         if user == "stats":
             print(f"Wins: {wins} Losses: {losses} Ties: {ties}")
             continue
@@ -25,29 +27,36 @@ def main():
             return (wins, losses, ties)
         if user == "rock" and computer == "scissors":
             print("You win")
+            win = True
             wins += 1
         elif user == "paper" and computer == "rock":
-                print("You win")
+                win = True
                 wins += 1
         elif user == "scissor"  or user == "scissors" and computer == "paper":
-                print("You win")
+                win = True
                 wins += 1
         elif user == computer:
             print("Its a tie")
             ties += 1
-        elif user not in ["rock", "paper", "scissor","scissors","stats"]:
+        elif user not in ["rock", "paper", "scissor","scissors","stats","exit"]:
             print("Choose a valid option")
             continue
         else:
             print("You lose")
+            lose = True
             losses += 1
-if __name__ == '__main__':
+        print(f"You chose {user} and the computer chose {computer} and you {'won' if win else 'lost' if lose else 'tied'}!")
+def main():
     while True:
-        user = input("start game? ")
+        user: str = input("Menu Options: start game?(y/n) or exit? ").lower()
         if user == "yes" or user == "y":
-           game = main()
-           break
+            game = running_game()
+            wins, losses, ties = game
+            print(f'Wins: {wins} Losses: {losses} Ties: {ties}')
+            break
+        elif user == "exit":
+            break
         else:
             print("Invalid Option")
-    wins, losses, ties = game
-print(f'Wins: {wins} Losses: {losses} Ties: {ties}')
+if __name__ == '__main__':
+    main()
